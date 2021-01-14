ac59c5224e8159d73a8954efec3aebcc1634f55b
agust <acanalis@itba.edu.ar>
2021-01-14 20:42:07 -0300

    Regenerate site

--- docs/js/wowchemy-init.min.285803d21d0d9cbb186d08eed0a1bc9d.js
+++ docs/js/wowchemy-init.min.285803d21d0d9cbb186d08eed0a1bc9d.js
@@ -3,7 +3,7 @@
 /*! License: https://github.com/wowchemy/wowchemy-hugo-modules/blob/master/LICENSE.md */
 
 ;
-(()=>{var body=document.body;function getThemeMode(){return parseInt(localStorage.getItem("wcTheme")||2);}
+(()=>{const body=document.body;function getThemeMode(){return parseInt(localStorage.getItem("wcTheme")||2);}
 function canChangeTheme(){return Boolean(window.wc.darkLightEnabled);}
 function initThemeVariation(){if(!canChangeTheme()){console.debug("User theming disabled.");return{isDarkTheme:window.wc.isSiteThemeDark,themeMode:window.wc.isSiteThemeDark?1:0};}
 console.debug("User theming enabled.");let isDarkTheme;let currentThemeMode=getThemeMode();console.debug(`User's theme variation: ${currentThemeMode}`);switch(currentThemeMode){case 0:isDarkTheme=false;break;case 1:isDarkTheme=true;break;default:if(window.matchMedia("(prefers-color-scheme: dark)").matches){isDarkTheme=true;}else if(window.matchMedia("(prefers-color-scheme: light)").matches){isDarkTheme=false;}else{isDarkTheme=window.wc.isSiteThemeDark;}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/acanalis/acanalis.github.io
 
 go 1.15
+
+require github.com/wowchemy/wowchemy-hugo-modules/wowchemy v0.0.0-20210111235350-f1da2c5ca88d // indirect
